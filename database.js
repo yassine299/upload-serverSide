0330772c765fb389428b1f173ba018df41c6621e
yassine299 <yassinehajjoubi299@gmail.com>
2021-07-06 13:26:04 +0100

    databse link

--- database.js
+++ database.js
@@ -1,8 +1,9 @@
 'use strict';
 const mongoose = require('mongoose');
+require("dotenv/config")
 
 module.exports = () => {
-    mongoose.connect('mongodb+srv://test:fuck1234@cluster0.qycyo.mongodb.net/test', {
+    mongoose.connect(process.env.DB_CONNECTION, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true,
